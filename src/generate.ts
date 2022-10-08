2dd53130053a8d90d78f9becc703968b1ba88d7c
andreyverbovskiy <56063258+andreyverbovskiy@users.noreply.github.com>
2022-10-08 20:46:36 +0300

    Update generate.ts

--- src/generate.ts
+++ src/generate.ts
@@ -1,5 +1,10 @@
+/// Install faker js using: 
+/// npm install @faker-js/faker --save-dev
+
+
 import { faker } from '@faker-js/faker';
 
+/* used for testing single outputs
 const fullName = `${faker.name.firstName()} ${faker.name.lastName()}`;
 const avatarUrl = faker.image.avatar();
 const natureImageUrl = faker.image.nature();
@@ -24,37 +29,26 @@ const time = faker.date.between(
   '2020-08-01T00:00:00.000Z',
   '2023-03-01T00:00:00.000Z'
 );
-
-/*export const USERS: User[] = [];
-export function lulz(): User {
-  return {
-    userId: faker.datatype.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
-}
 */
-class User {}
 
-function createRandomUser(): User {
+class User {} /// create class for the data
+
+function createRandomUser(): User { ///generate data for each delivery
   return {
-    storage_id: faker.helpers.arrayElement(['Storage A', 'Storage B']),
-    price: faker.finance.amount(),
-    Item_ID: faker.internet.password(),
-    Item_name: faker.commerce.product(),
+    storage_id: faker.helpers.arrayElement(['Storage A', 'Storage B']), ///selects values only from array, atm we plan to have only one storage, but added two just in case
+    price: faker.finance.amount(), ///price for delivery
+    Client_ID: faker.datatype.uuid(), ///unique user id...maybe limit options??? Otherwise all users will be unique
+    Item_ID: faker.internet.password(), ///unique id of a devilvery-item, used another style of id generation
+    Item_name: faker.commerce.product(), ///name of the product (not necessary) example: T-shirt, wallet...
     User_address: faker.address.nearbyGPSCoordinate(
       [60.192059, 24.945831],
       20,
       true
     ), // 20 km area from the center of Helsinki (most likely need to change city as Helsinki is close to water)
-    Client_ID: faker.datatype.uuid(), //maybe limit options??? Otherwise all users will be unique
     time: faker.date.between(
       '2020-08-01T00:00:00.000Z',
       '2023-03-01T00:00:00.000Z'
-    ),
+    ), ///random day/time between the dates (943 days in total). 2 years of past data and a few month ahead from today
     status: faker.helpers.arrayElement(['On the way', 'Delivered']),
     // can also add names of users, zipcode, email, properties of package (size,weight...), also can add avatar picture
     // need to think of time it took to deliver
@@ -64,6 +58,7 @@ function createRandomUser(): User {
 let user_values: User[] = []; // array of objects to store data
 const user = createRandomUser(); //single user (was used for testing)
 
+///data generation:
 Array.from({ length: 10 }).forEach(() => {
   //change length parameter to define the data size, most likely we will have around 70 deliveries per day, we have 943 days -> 66 010 data samples
   user_values.push(createRandomUser());
@@ -72,9 +67,14 @@ Array.from({ length: 10 }).forEach(() => {
 /// values can be reached with user_values[<number in array>].<class parameter>
 /// Example of outputing value through HTML: ${user_values[1].price}
 
-///Array.from({ length: 10 }).forEach(() => {
-///  USERS.push(createRandomUser());
-///});
+
+
+
+
+///The following code was used to test generated data through HTML page
+
+
+
 
 const appDiv: HTMLElement = document.querySelector('#app');
 appDiv.innerHTML = `
