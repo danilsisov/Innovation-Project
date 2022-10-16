125714763a4d3a64b8ae33e88d0719d766e21686
dotdm26 <thaiducminhdo@gmail.com>
2022-10-16 20:15:32 +0300

    Automate data gen & update

--- Data Display Demo/server/index.js
+++ Data Display Demo/server/index.js
@@ -9,11 +9,14 @@ import PackageModel from "./models/packages.js";
 app.use(express.json());
 app.use(cors());
 
+await mongoose.connect("mongodb+srv://minhdo:SI-E_Metropolia2019.@madd-cluster.d0ozgqq.mongodb.net/packages-database?retryWrites=true&w=majority");
+
 //Type ES Module
 
+//add more logic (e.g. if it's unshipped then give it no storage ID)
+//maybe keep everything as unshipped at the start?
 async function seedDB() {
     try {
-        await mongoose.connect("mongodb+srv://minhdo:SI-E_Metropolia2019.@madd-cluster.d0ozgqq.mongodb.net/packages-database?retryWrites=true&w=majority");
         //generate new package
         let newPackage = {
             storage_id: faker.helpers.arrayElement(['Storage A', 'Storage B', 'User address']),
@@ -34,18 +37,41 @@ async function seedDB() {
                 '2020-08-01T00:00:00.000Z',
                 '2023-03-01T00:00:00.000Z'
             ),
-            status: faker.helpers.arrayElement(['On the way', 'Delivered', 'Sorting', 'Not received by delivery']),
+            status: faker.helpers.arrayElement([/*'On the way', 'Delivered', 'Sorting', */'Unshipped']),
             // can also add names of users, zipcode, email, properties of package (size,weight...), also can add avatar picture
             // need to think of time it took to deliver
         }
         const new_package = new PackageModel(newPackage);
         await new_package.save();
-        console.log("working");
+        console.log("Generated new data");
     }
     catch (err) {
         console.log(err.stack);
     }
 }
+
+async function dataUpdate() {
+    let check = Math.floor(Math.random() * 4);
+    let updated;
+    switch (check) {
+        //PROBLEM: findoneandupdate updates everything
+        case 0:
+            updated = await PackageModel.findOneAndUpdate({status: "Unshipped"} , {status: "Sorting"});
+        case 1:
+            updated = await PackageModel.findOneAndUpdate({status: "Sorting"} , {status: "On the way"});
+        case 2: //in case it goes through another checkpoint
+            updated = await PackageModel.findOneAndUpdate({status: "On the way"} , {status: "Sorting"});
+        case 3:
+            updated = await PackageModel.findOneAndUpdate({status: "On the way"} , {status: "Delivered"});
+    }
+    console.log("Updated case " + check + "\n" + updated)
+}
+
+//generate new data every 30s
+setInterval(seedDB, 30000);
+
+//update preexisting data every 20s
+setInterval(dataUpdate, 20000);
 
 app.get("/getPackages", (req, res) => {
     PackageModel.find({}, (err, result) => {
@@ -60,9 +86,10 @@ app.get("/getPackages", (req, res) => {
     });
 });
 
+/*
 app.get("/gen", async () => {
     await seedDB();
-})
+})*/
 
 app.listen(3001, () => {
     console.log("Server is running at port 3001");
